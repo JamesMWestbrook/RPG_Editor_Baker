bc7a64fa17557b8e116fb20f6429940458205469
JamesMWestbrook <james.michael.westbrook@gmail.com>
2025-08-20 18:48:55 -0500

    disconnecting nodes

--- Event/event_graph_edit.gd
+++ Event/event_graph_edit.gd
@@ -19,8 +19,8 @@ func _on_connection_to_empty(from_node: StringName, from_port: int, release_posi
 	_from_node = from_node
 	_from_port = from_port
 	
-	_show_context_menu()
 	mouse_position = get_global_mouse_position()
+	_show_context_menu()
 	
 func _show_context_menu():
 	popup_menu.position = mouse_position
@@ -34,7 +34,7 @@ func _on_message_popup_menu_index_pressed(index: int) -> void:
 			new_node = MESSAGE_NODE.instantiate()
 			add_child(new_node)
 			new_node.position_offset = mouse_position
-
+			connect_node(_from_node,_from_port,new_node.name, 0)
 
 func _on_connection_request(from_node: StringName, from_port: int, to_node: StringName, to_port: int) -> void:
 	var from = get_node_or_null(NodePath(from_node))
@@ -58,3 +58,7 @@ func _on_gui_input(event: InputEvent) -> void:
 	if event.is_action_pressed("right_click"):
 		mouse_position = get_global_mouse_position()
 		_show_context_menu()
+
+
+func _on_disconnection_request(from_node: StringName, from_port: int, to_node: StringName, to_port: int) -> void:
+	disconnect_node(from_node,from_port, to_node, to_port)
